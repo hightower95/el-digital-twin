f7c7c2308b9608dda57942467b6de70bcc241468
High Tower <>
2025-07-06 23:46:49 +0200

    function rename

--- src/el_analysis/connector_toolkit/utils.py
+++ src/el_analysis/connector_toolkit/utils.py
@@ -1,6 +1,16 @@
-from el_analysis import config, logging
 
-def get_connector_by_part_number(part_number: str):
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from el_analysis.models.physical.connector import Connector
+
+from el_analysis import config, logging
+from typing import Dict
+
+__connector_cache: Dict[str, Connector] = {}
+
+def get_connector_from_part_number(part_number: str):
     """
     Retrieves a connector by its part number.
     
@@ -17,6 +27,10 @@ def get_connector_by_part_number(part_number: str):
 
     if not part_number:
         raise ValueError("Part number cannot be empty.")
+    
+    connector = __connector_cache.get(part_number)
+    if connector:
+        return connector
 
     connector = Connector()
     connector.part.part_number = part_number    
